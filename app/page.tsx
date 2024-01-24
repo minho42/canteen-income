7c0e98288fd5a9f4d6906794749afcc2dba8f82a
Min ho Kim <minho42@gmail.com>
2024-01-24 17:53:28 +1100

    style

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ export default async function Page() {
     <div className="flex items-center justify-center">
       <main className="max-w-xl mb-20 px-2">
         <div className="flex flex-col items-center justify-center py-3">
-          <h1 className="font-bold text-xl text-center">NSW public school canteen income (2022)</h1>
+          <h1 className="font-semibold text-xl text-center">NSW public school canteen income (2022)</h1>
 
           <a
-            className=""
+            className="text-blue-600 underline"
             href="https://data.cese.nsw.gov.au/data/dataset/nsw-public-schools-income-from-licensed-canteens"
           >
             source
@@ -31,7 +31,7 @@ export default async function Page() {
                   <div className="flex items-center justify-center py-3">
                     <div className="font-medium text-center w-10">{index + 1}</div>
                   </div>
-                  <div className="flex flex-wrap flex-shrink w-full font-semibold text-left ">{row.name}</div>
+                  <div className="flex flex-wrap flex-shrink w-full text-left ">{row.name}</div>
                   <div className="text-right font-mono text-sm bg-indigo-100 rounded-md px-2 py-1">
                     {row.income}
                   </div>
